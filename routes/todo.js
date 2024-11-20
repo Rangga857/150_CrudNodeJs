579fbc0d2922763a176bc6ac764df5a413fc51f9
Rangga857 <azharrangga28@gmail.com>
2024-11-20 11:07:07 +0700

    Middleware

--- routes/todo.js
+++ routes/todo.js
@@ -2,17 +2,17 @@ const express = require('express');
 
 const router = express.Router();
 
-let todos = [{
-        id: 1,
-        task: 'Belajar Node.js',
-        completed: false
-    },
-    {
-        id: 2,
-        task: 'Membuat API',
-        completed: false
-    },
-];
+// let todos = [{
+//         id: 1,
+//         task: 'Belajar Node.js',
+//         completed: false
+//     },
+//     {
+//         id: 2,
+//         task: 'Membuat API',
+//         completed: false
+//     },
+// ];
 
 router.get('/', (req, res) => { res.json(todos); });
 
